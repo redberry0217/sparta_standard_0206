720ed6b608818beba6e86e22b58ebf3eea0864ab
redberry0217 <redberry0217@gmail.com>
2024-02-06 15:05:57 +0900

    0206 ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+import { addNumber } from "./redux/modules/calculator";
+import { subtractNumber } from "./redux/modules/calculator";
 
 function App() {
+  const dispatch = useDispatch();
+  const [number, setNumber] = useState(0);
+  const resultNum = useSelector((state) => state.calculator.number);
+
+  const addBtnHandler = () => {
+    dispatch(addNumber(number));
+    setNumber(0);
+  };
+
+  const strBtnHandler = () => {
+    dispatch(subtractNumber(number));
+    setNumber(0);
+  };
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        만큼을 <button onClick={addBtnHandler}>더할게요</button>{" "}
+        <button onClick={strBtnHandler}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{resultNum}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,44 @@
+export const ADD_NUM = "ADD_NUM";
+export const STR_NUM = "STR_NUM";
+
+export const addNumber = (payload) => {
+  return {
+    type: ADD_NUM,
+    payload,
+  };
+};
+
+export const subtractNumber = (payload) => {
+  return {
+    type: STR_NUM,
+    payload,
+  };
+};
+
 // 초기 상태값
 const initialState = {
   number: 0,
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NUM:
+      return {
+        ...state,
+        number: parseInt(state.number) + parseInt(action.payload),
+      };
+
+    case STR_NUM:
+      return {
+        ...state,
+        number: parseInt(state.number) - parseInt(action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
